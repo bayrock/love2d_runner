59815c0ed257e1c88ec16ccc828bb345b1e432f5
bayrock <bayrock14@yahoo.com>
2014-11-04 21:34:43 -0500

    Added song loop

--- game/game.lua
+++ game/game.lua
@@ -6,12 +6,12 @@ Author: Bayrock (http://Devinity.org)
 require("game.player")
 require("game.entity")
 
-local function round(num, idp) -- Function to round to the nearest whole number or decimal
+local function round(num, idp) -- round to the nearest whole number or decimal
 	local mult = 10^(idp or 0)
 	return math.floor(num * mult + 0.5) / mult
 end
 
-function game.reload()
+function game.Reload()
 	for k, v in pairs(ent.GetAll()) do v:Kill() end
 	frequency = 4
 	nextIncrement = 10
@@ -23,10 +23,10 @@ function game.reload()
 end
 
 lg = love.graphics
-function game:draw()
-	player.draw() -- Draw player
+function game.Draw()
+	player.Draw() -- draw player
 
-	if debug then -- Draw debug
+	if debug then -- draw debug
 		lg.print(projectName..version, 5, 5)
 		lg.print("FPS: "..love.timer.getFPS( ), 5, 20)
 		lg.print("Player X: "..math.floor(player.x), 5, 35)
@@ -37,22 +37,30 @@ function game:draw()
 		lg.rectangle("line", player.x - 25, player.y - 33.3, 50, 50) -- draw hitbox outline
 	end
 
-	if player.dead then -- Draw game over
+	if player.dead then -- draw game over
 		lg.setColor(0, 51, 51)
 		lg.rectangle("fill", 0, 0, love.window.getWidth(), love.window.getHeight())
 		lg.setColor(255, 255, 255)
 		lg.printf("Game over!", love.window.getWidth()/4, love.window.getHeight()/2.5, 350, "center")
-		lg.printf("Press any key to replay", love.window.getWidth()/4, love.window.getHeight()/2.5 + 15, 350, "center")
+		lg.printf("You scored: "..round(player.score), love.window.getWidth()/4, love.window.getHeight()/2.5 + 15, 350, "center")
+		lg.printf("Press any key to replay", love.window.getWidth()/4, love.window.getHeight()/2.5 + 45, 350, "center")
 	end
 end
 
-function UPDATE_GAME(dt) -- Called by love.update constantly
-	for k,v in pairs(ent.GetAll()) do v:Update() end
-	player.move(dt)
-	player.score = player.score + dt
+function game.LoadAudio()
+	local volume = love.audio.getVolume()
+	gameloop = love.audio.newSource("sound/gameloop.mp3")
+	gameloop:setLooping(true)
+	love.audio.setVolume(volume * 0.15)
 end
 
-function DRAW_GAME() -- Draws to the screen
+function UPDATE_GAME(dt) -- called by love.update
+	for k,v in pairs(ent.GetAll()) do v:Update() end
+	player.Update(dt)
+	ent.Update(dt)
+end
+
+function DRAW_GAME() -- called by love.draw
 	for k,v in pairs(ent.GetAll()) do v:Draw() end
-	game:draw()
+	game.Draw()
 end
