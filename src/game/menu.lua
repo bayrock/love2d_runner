73d9d49f796359462009ae71a52aa1b36c5544b8
Bailey Chestnut <bayrock@devinity.org>
2014-12-27 07:59:12 -0500

    Updated menu animation
    and added to dead screen

--- src/game/menu.lua
+++ src/game/menu.lua
@@ -22,28 +22,36 @@ function menu:init()
 	menuloop:play()
 end
 
-local alphaAnim = 253
+local alpha = 253
 function menu:draw()
 	local w,h = windowWidth, windowHeight
 	lg.setFont(font(32))
 	lg.setColor(153,153,255)
 	lg.printf(projectName..version, 0, h/2 - 40, w, "center")
-	lg.setColor(255,255,255, alphaAnim)
+	lg.setColor(255,255,255, alpha)
 	lg.printf("Press space to play", 0, h/2, w, "center")
 end
 
-local anim = false
+function menu:enter()
+	alpha = 255
+end
+
+local animate = false
+function anim(dt)
+	if alpha >= 254 then
+		animate = false
+		elseif alpha <= 30 then
+			animate = true
+		end
+		if animate then
+			alpha = alpha + 90 * dt
+		else
+			alpha = alpha - 90 * dt
+	end
+end
+
 function menu:update(dt)
-	if alphaAnim >= 254 then
-		anim = false
-	elseif alphaAnim <= 30 then
-		anim = true
-	end
-	if anim then
-		alphaAnim = alphaAnim + 90 * dt
-	else
-		alphaAnim = alphaAnim - 90 * dt
-	end
+	anim(dt)
 end
 
 function menu:keyreleased(key)
@@ -56,6 +64,7 @@ end
 dead = {} -- dead state constructor
 function dead:enter()
 	lg.setBackgroundColor(0, 51, 51)
+	alpha = 255
 	gameloop:stop()
 	menuloop:play()
 	entKillAll()
@@ -71,6 +80,7 @@ function dead:draw()
 	lg.setFont(font(32))
 	lg.printf("Game over!", 0, h/2 - 80, w, "center")
 	lg.printf("You scored: "..round(player.score, 1), 0, h/2 - 35, w, "center")
+	lg.setColor(255,255,255, alpha)
 	lg.printf("Space to replay", 0, h/2 + 30, w, "center")
 	lg.printf("backspace for menu", 0, h/2 + 50, w, "center")
 	if newHighscore then
@@ -79,6 +89,10 @@ function dead:draw()
 	else
 		lg.printf("Highscore: "..round(player.highscore, 1), 0, h/2 - 15, w, "center")
 	end
+end
+
+function dead:update(dt)
+	anim(dt)
 end
 
 function dead:keypressed(key)
